dbdd7f68c0ff354fd08c61d8c52dab1d033a982c
deindedawodu <mr.dawodu@gmail.com>
2017-02-10 13:41:25 -0500

    wiring up reducers, action creators, and cards

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import UserList from './user_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      	<h1>Lets Go Middleware!!!</h1>
-      Ahight!!!!
+      	<UserList/>
       </div>
     );
   }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,1 +1,11 @@
-import  {FETCH_USERS} from '../src/actions/types';
+import  {FETCH_USERS} from '../actions/types';
+
+
+
+export default  function(state=[],action){
+	switch(action.type){
+   case FETCH_USERS:
+    return [...state, ...action.payload]
+	}
+	return state;
+}
